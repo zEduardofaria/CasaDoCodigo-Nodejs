67f1ab3f94379bb5efa45f40aad75e860dd41564
Eduardo Faria <z.eduardofaria@gmail.com>
2019-09-15 11:40:33 -0300

    Livros crud finished

--- src/app/infra/livro-dao.js
+++ src/app/infra/livro-dao.js
@@ -4,6 +4,32 @@ class LivroDao {
         this._db = db;
     }
 
+    adiciona(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                INSERT INTO livros (
+                    titulo, 
+                    preco,
+                    descricao
+                ) values (?,?,?)
+                `,
+                [
+                    livro.titulo,
+                    livro.preco,
+                    livro.descricao
+                ],
+                function (err) {
+                    if (err) {
+                        console.log(err);
+                        return reject('Não foi possível adicionar o livro!');
+                    }
+
+                    resolve();
+                }
+            )
+        });
+    }
+
     lista() {
         return new Promise((resolve, reject) => {
             this._db.all(
@@ -14,6 +40,71 @@ class LivroDao {
                     return resolve(resultados);
                 }
             )
+        });
+    }
+
+    buscaPorId(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    SELECT *
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro, livro) => {
+                    if (erro) {
+                        return reject('Não foi possível encontrar o livro!');
+                    }
+                    return resolve(livro);
+                }
+            );
+        });
+    }
+
+    atualiza(livro) {
+        return new Promise((resolve, reject) => {
+            this._db.run(`
+                UPDATE livros SET
+                titulo = ?,
+                preco = ?,
+                descricao = ?
+                WHERE id = ?
+            `,
+            [
+                livro.titulo,
+                livro.preco,
+                livro.descricao,
+                livro.id
+            ],
+            erro => {
+                if (erro) {
+                    return reject('Não foi possível atualizar o livro!');
+                }
+
+                resolve();
+            });
+        });
+    }
+
+    remove(id) {
+
+        return new Promise((resolve, reject) => {
+            this._db.get(
+                `
+                    DELETE 
+                    FROM livros
+                    WHERE id = ?
+                `,
+                [id],
+                (erro) => {
+                    if (erro) {
+                        return reject('Não foi possível remover o livro!');
+                    }
+                    return resolve();
+                }
+            );
         });
     }
 }
--- src/app/rotas/rotas.js
+++ src/app/rotas/rotas.js
@@ -28,15 +28,50 @@ module.exports = (app) => {
                     }
                 ))
                 .catch(erro => console.log(erro));
-        // livroDao.lista(function(erro, resultados) {
+    });
 
-        //     resp.marko(
-        //         require('../views/livros/lista/lista.marko'),
-        //         {
-        //             livros: resultados
-        //         }
-        //     );
+    app.get('/livros/form', function(req, resp) {
+        resp.marko(require('../views/livros/form/form.marko'), { livro: {} });
+    });
 
-        // });
+    app.get('/livros/form/:id', function(req, resp) {
+        const id = req.params.id;
+        const livroDao = new LivroDao(db);
+
+        livroDao.buscaPorId(id)
+                .then(livro => 
+                    resp.marko(
+                        require('../views/livros/form/form.marko'), 
+                        { livro: livro }
+                    )
+                )
+                .catch(erro => console.log(erro));
+    });
+
+    app.post('/livros', function(req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+        
+        livroDao.adiciona(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
+    });
+
+    app.put('/livros', function(req, resp) {
+        console.log(req.body);
+        const livroDao = new LivroDao(db);
+        
+        livroDao.atualiza(req.body)
+                .then(resp.redirect('/livros'))
+                .catch(erro => console.log(erro));
+    });
+
+    app.delete('/livros/:id', function(req, resp) {
+        const id = req.params.id;
+
+        const livroDao = new LivroDao(db);
+        livroDao.remove(id)
+                .then(() => resp.status(200).end())
+                .catch(erro => console.log(erro));
     });
 };
