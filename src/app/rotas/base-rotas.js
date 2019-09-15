cc8d39940adab72fc8e461b9cfc05ec79a37751f
Eduardo Faria <z.eduardofaria@gmail.com>
2019-09-15 18:39:28 -0300

    MVC pattern added

--- src/app/rotas/base-rotas.js
+++ src/app/rotas/base-rotas.js
@@ -5,8 +5,4 @@ module.exports = (app) => {
     const rotasBase = BaseControlador.rotas();
 
     app.get(rotasBase.home, baseControlador.home());
-
-    app.route(rotasBase.login)
-        .get(baseControlador.login())
-        .post(baseControlador.efetuaLogin());
 };
--- src/app/views/base/index.js
+++ src/app/views/base/index.js
@@ -1,6 +1,5 @@
 module.exports = {
     erro404: require('./erros/404.marko'),
     erro500: require('./erros/500.marko'),
-    home: require('./home/home.marko'),
-    login: require('./login/login.marko')
-}
+    home: require('./home/home.marko')
+};
