9f876f65cb8baa82dd6302c39936a4b96ecb933d
Eduardo Faria <z.eduardofaria@gmail.com>
2019-09-15 19:54:39 -0300

    Authentication added

--- src/app/controladores/base-controlador.js
+++ src/app/controladores/base-controlador.js
@@ -1,10 +1,13 @@
+const LivroControlador = require('./livro-controlador');
+
 const templates = require('../views/templates');
 
 class BaseControlador {
 
     static rotas() {
         return {
-            home: '/'
+            home: '/',
+            login: '/login'
         };
     }
 
@@ -14,6 +17,37 @@ class BaseControlador {
                 templates.base.home
             );
         };
+    }
+
+    login() {
+
+        return function(req, resp) {
+            resp.marko(templates.base.login);
+        };
+    }
+
+    efetuaLogin() {
+
+        return function(req, resp, next) {
+            const passport = req.passport;
+            passport.authenticate('local', (erro, usuario, info) => {
+                if (info) {
+                    return resp.marko(templates.base.login);
+                }
+
+                if (erro) {
+                    return next(erro);
+                }
+
+                req.login(usuario, () => {
+                    if (erro) {
+                        return next(erro);
+                    }
+
+                    return resp.redirect(LivroControlador.rotas().lista);
+                });
+            })(req, resp, next);
+        }
     }
 }
 
